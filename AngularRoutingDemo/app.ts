11ed10075d1348a3797cb2c6ceeae5e684dc0d72
Nat Ayewah <nayewah@live.com>
2015-08-31 17:57:27 -0700

    initial commit

---
+++ AngularRoutingDemo/app.ts
@@ -0,0 +1,56 @@
+﻿
+
+class App {
+    static main() {
+        angular
+            .module("RouteDemo", ['ngRoute'])
+            .controller("AppCtrl", AppCtrl)
+            .config(this.ConfigRoutes)      
+    }
+
+    static ConfigRoutes($routeProvider: ng.route.IRouteProvider) {
+        $routeProvider
+            .when("/home", {
+                action: "home.default"
+            })
+            .when("/range/:line/:column", {
+                action: "range.line"
+            })
+            .when("/search/:term", {
+                action: "search.term"
+            })
+            .when("/search/:term/select/:result", {
+                action: "search.term.result"
+            })
+    }
+}
+
+class AppCtrl {
+    // alias services with 'any' type
+    private _route: any;
+    private _routeParams: any;
+
+    constructor(
+        private $scope: ng.IScope,
+        private $route: ng.route.IRoute,
+        private $routeParams: ng.route.IRouteParamsService 
+        ) {
+        this._route = $route;
+        this._routeParams = $routeParams;
+
+        $scope.$on("$routeChangeSuccess",
+            ($currentRoute, $previousRoute) => {
+                this.updateState();
+            });
+    }
+
+    updateState() {
+        var current = this._route.current;
+        console.log("$route.current", current && current.$$route );
+        console.log("$routeParams", this.$routeParams);
+    }
+}
+
+
+//$(() => App.main());
+App.main();
